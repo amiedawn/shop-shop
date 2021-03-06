c5ad1e050e797055167727845b445e2ec08644ea
amiedawn <amie.d.carroll@gmail.com>
2021-03-06 10:33:41 -0700

    working on state

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,6 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
